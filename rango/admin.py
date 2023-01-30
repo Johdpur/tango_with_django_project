aecc27f0e6dc8b34e5cd0dfd66b00e15d8dda3ea
Josh Duffy <2662611d@student.gla.ac.uk>
2023-01-30 12:45:28 +0000

    Completed Ch6.3

--- rango/admin.py
+++ rango/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-#admin.site.register(Page)
+# admin.site.register(Category)
+# admin.site.register(Page)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
 
 admin.site.register(Page, PageAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
 
 """
 class PageInline(admin.TabularInline):
@@ -21,7 +28,7 @@ class CategoryAdmin(admin.ModelAdmin):
     ]
     inlines = [PageInline]
     
-
-admin.site.register(Category, CategoryAdmin)
 """
+
+
 
