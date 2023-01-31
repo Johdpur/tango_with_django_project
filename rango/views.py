c0ec74bc8caefb0017053160f54b2d4958d8d3d5
Josh Duffy <2662611d@student.gla.ac.uk>
2023-01-31 21:45:53 +0000

    Completed Ch7 - Passes Ch7 test

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
 
 
 def index(request):
@@ -74,6 +76,9 @@ def add_category(request):
         if form.is_valid():
             # Save the new category to the database.
             form.save(commit=True)
+            # cat = form.save(commit=True)
+            # print(cat, cat.slug)
+            
             # Now that the category is saved, we could confirm this.
             # For now, just redirect the user back to the index view.
             return redirect('/rango/')
@@ -87,5 +92,31 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None    
+    
+    if category is None:
+        return redirect('/rango/')
+    
+    form = PageForm()
+    
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.category = category
+            page.views = 0
+            page.save()
+            
+            return redirect(reverse('rango:show_category', kwargs={'category_name_slug' : category_name_slug}))
+        else:
+            print(form.errors)
+            
+    context_dict = {'form' : form, 'category' : category}
+    return render(request, 'rango/add_page.html', context=context_dict)
 
 
